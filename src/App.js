32b0eb9b7463a032202e939db1855cedfd39f52c
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-22 11:03:55 -0400

    implementação do redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import Header from './components/Header';
 import Routes from './routes'
+
+import store from './store'
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Header />
-      <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
