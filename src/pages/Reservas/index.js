8f0646122c6d3d7ebad2b21f583cdf1ddf005113
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-26 16:45:59 -0400

    Consultando o estoque no componente

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { removeReserve, updateAmountReserve } from '../../store/modules/reserve/actions'
+import { removeReserve, updateAmountRequest } from '../../store/modules/reserve/actions'
 import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md'
 
 import './styles.css'
@@ -14,11 +14,11 @@ const Reservas = () => {
   }
 
   function decrementAmount(trip){
-    dispatch(updateAmountReserve(trip.id, trip.amount -1));
+    dispatch(updateAmountRequest(trip.id, trip.amount -1));
   }
 
   function incrementAmount(trip){
-    dispatch(updateAmountReserve(trip.id, trip.amount +1));
+    dispatch(updateAmountRequest(trip.id, trip.amount +1));
   }
 
   return (
--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,5 +1,5 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
-import { addReserveSuccess, updateAmountReserve } from './actions';
+import { addReserveSuccess, updateAmountSuccess } from './actions';
 import api from '../../../services/api'
 
 function* addToReserve({ id }) {
@@ -21,7 +21,7 @@ function* addToReserve({ id }) {
   if (tripExists) {
     const amount = tripExists.amount + 1;
 
-    yield put(updateAmountReserve(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `trips/${id}`);
 
@@ -35,6 +35,21 @@ function* addToReserve({ id }) {
 
 }
 
+function* updateAmount({id, amount}){
+  if(amount <= 0) return;
+
+  const myStock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  if(amount > stockAmount) {
+    alert('Quantidade maxima atingida.');
+    return;
+  }
+  yield put(updateAmountSuccess(id, amount));
+}
+
 export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
+  takeLatest('ADD_RESERVE_REQUEST', addToReserve),
+  takeLatest('UPDATE_RESERVE_REQUEST', updateAmount),
 ])
